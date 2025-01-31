abe2760c68456db55b56eb1d508d88c433bd9837
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-01-31 15:14:43 +0530

    built script for sys-config

---
+++ os-config/os.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	// "github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+func main() {
+	fmt.Println("🖥️ Displaying system information:")
+	fmt.Println("----------------------")
+	fmt.Println("Operating System : ", runtime.GOOS)
+	fmt.Println("Architecture : ", runtime.GOARCH)
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Hostname : ", hostname)
+	}
+
+	cpuInfo, err := cpu.Info()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Cpu : ", cpuInfo[0].ModelName)
+		fmt.Println("Cores : ", len(cpuInfo))
+
+	}
+
+	vmStat, err := mem.VirtualMemory()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Total RAM: %.2f GB\n", float64(vmStat.Total)/1e9)
+		fmt.Printf("Available RAM: %.2f GB\n", float64(vmStat.Available)/1e9)
+	}
+
+	// hostStat, err := host.Info()
+	// if(err != nil){
+	// 	panic(err)
+	// }else{
+	// 	fmt.Printf("Uptime: %d seconds\n", hostStat.Uptime)
+	// 	fmt.Printf("Platform: %s %s\n", hostStat.Platform, hostStat.PlatformVersion)
+	// }
+
+	fmt.Println("----------------------")
+}
